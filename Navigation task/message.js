7c19769f4edc9ba897281254cc59f6d860576ba0
Voona Reshmanth <reshmanth.voona@corp.knowledgelens.com>
2023-05-26 18:58:32 +0530

    NavTsk

---
+++ Navigation task/message.js
@@ -0,0 +1,21 @@
+
+function link_page(sectionId) {
+    window.open(sectionId, "_blank");
+}
+
+function dashboard() {
+    document.write("Dashboard")
+}
+
+function user_details() {
+    document.write("User Details")
+}
+
+function user_list() {
+    document.write("User List")
+}
+
+function user_setup() {
+    document.write("User Setup")
+}
+
